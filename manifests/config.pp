8a810c117a77b4339703271b75901d089fee64ac
kreczko <lkreczko@gmail.com>
2013-07-23 16:01:44 +0100

    adding config files and templates as well as the config manifest

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,6 +1,21 @@
 # Class htcondor::config
 #
 # Configuration deployment for HTCondor
-class htcondor::config {
-  
+class htcondor::config (
+  $is_worker   = false,
+  $condor_host = $fqdn,
+  $allow_write = [
+    ]) {
+  $template = "${module_name}/condor_config_CE.local.erb"
+
+  if $is_worker {
+    $template = "${module_name}/condor_config_worker.local.erb"
+  }
+
+  file { '/etc/condor/condor_config.local':
+    ensure  => present,
+    backup  => ".bak",
+    content => template($template),
+  }
+
 }
