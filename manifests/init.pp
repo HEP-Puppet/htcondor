71ce917bbb379f3640658108dd919f70e59560ae
kreczko <lkreczko@gmail.com>
2013-07-23 16:07:13 +0100

    adding parameters to init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,14 +11,23 @@
 # Sample Usage:
 #
 class htcondor (
-  $install_repositories = true,) {
+  $install_repositories = true,
+  $is_worker            = false,
+  $condor_host          = $fqdn,
+  $allow_write          = [
+    ]) {
   class { 'htcondor::repositories': install_repos => $install_repositories, }
 
   class { 'htcondor::install': }
 
-  class { 'htcondor::config': }
+  class { 'htcondor::config':
+    is_worker   => $is_worker,
+    condor_host => $condor_host,
+    allow_write => $allow_write,
+  }
 
-  class { 'htcondor::service': }
+  class { 'htcondor::service':
+  }
 
   Class['htcondor::repositories'] -> Class['htcondor::install'] -> Class['htcondor::config'] -> Class['htcondor::service'
     ]
