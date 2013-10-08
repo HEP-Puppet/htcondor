45d4f89a05d2fad865086427f8ab6e1d28aa544e
Luke Kreczko <lkreczko@gmail.com>
2013-10-08 16:16:39 +0100

    changing the template for condor_config.local (now not only email)

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,11 +6,12 @@ class htcondor::config (
   $is_ce              = false,
   $is_manager         = false,
   $condor_host        = $fqdn,
-
   # pool_password can also be served from central file location using hiera
   $pool_password      = 'puppet:///modules/${module_name}/pool_password',
-  #use if condor host has two NICs 
-  #and only the private should be used for condor
+  # use if condor host has two NICs
+  # and only the private should be used for condor
+  # use if condor host has two NICs
+  # and only the private should be used for condor
   $condor_host_ip     = '',
   $condor_admin_email = 'root@mysite.org',
   $collector_name     = 'Personal Condor at $(FULL_HOSTNAME)',
@@ -25,9 +26,7 @@ class htcondor::config (
   $now                 = strftime('%d.%m.%Y_%H.%M')
   $ce_daemon_list      = ['SCHEDD']
   $worker_daemon_list  = ['STARTD']
-  $manage_daemon_list  = [
-    'COLLECTOR',
-    'NEGOTIATOR']
+  $manage_daemon_list  = ['COLLECTOR', 'NEGOTIATOR']
 
   # default daemon, runs everywhere
   $default_daemon_list = ['MASTER']
@@ -38,12 +37,12 @@ class htcondor::config (
     ]
 
   # complex preparation of manager, computing_element and worker_nodes lists
-   $managers_with_uid_domain           = prefix($managers, 'condor_pool@$(UID_DOMAIN)/')
-   $computing_elements_with_uid_domain = prefix($computing_elements, 'condor_pool@$(UID_DOMAIN)/'
-   )
-   $worker_nodes_with_uid_domain       = prefix($worker_nodes, 'condor_pool@$(UID_DOMAIN)/'
-   )
-
+  $managers_with_uid_domain           = prefix($managers, 'condor_pool@$(UID_DOMAIN)/'
+  )
+  $computing_elements_with_uid_domain = prefix($computing_elements, 'condor_pool@$(UID_DOMAIN)/'
+  )
+  $worker_nodes_with_uid_domain       = prefix($worker_nodes, 'condor_pool@$(UID_DOMAIN)/'
+  )
 
   if $is_ce and $is_manager {
     # machine is both CE and manager (for small sites)
@@ -54,7 +53,7 @@ class htcondor::config (
       File['/etc/condor/config.d/21_schedd.config'],
       File['/etc/condor/config.d/22_manager.config'],
       ]
-    $config_files            = concat($common_config_files, 
+    $config_files            = concat($common_config_files,
     $additional_config_files)
   } elsif $is_ce {
     $daemon_list             = concat($default_daemon_list, $ce_daemon_list)
@@ -62,14 +61,12 @@ class htcondor::config (
       File['/etc/condor/config.d/12_resourcelimits.config'],
       File['/etc/condor/config.d/21_schedd.config'],
       ]
-    $config_files            = concat($common_config_files, 
+    $config_files            = concat($common_config_files,
     $additional_config_files)
-  } elsif  $is_manager {
+  } elsif $is_manager {
     # machine running only manager
     $daemon_list             = concat($default_daemon_list, $manage_daemon_list)
-    $additional_config_files = [
-      File['/etc/condor/config.d/22_manager.config'],
-      ]
+    $additional_config_files = [File['/etc/condor/config.d/22_manager.config'],]
     $config_files            = concat($common_config_files,
     $additional_config_files)
 
@@ -77,7 +74,7 @@ class htcondor::config (
     $daemon_list             = concat($default_daemon_list, $worker_daemon_list)
     $additional_config_files = [File['/etc/condor/config.d/20_workernode.config'
         ],]
-    $config_files            = concat($common_config_files, 
+    $config_files            = concat($common_config_files,
     $additional_config_files)
   } else {
     $daemon_list  = $default_daemon_list
@@ -93,7 +90,7 @@ class htcondor::config (
 
   file { '/etc/condor/condor_config.local':
     backup  => ".bak.${now}",
-    content => "CONDOR_ADMIN = $condor_admin_email\n",
+    content => template("${module_name}/condor_config.local.erb"),
     require => Package['condor'],
   # notify  => Service["condor"], this should be exec {'condor_reconfig':}
   }
@@ -103,10 +100,7 @@ class htcondor::config (
     require => Package['condor'],
   }
 
-  file { [
-    '/pool',
-    '/pool/condor',
-    '/etc/condor/persistent']:
+  file { ['/pool', '/pool/condor', '/etc/condor/persistent']:
     ensure => directory,
     owner  => 'condor',
   }
