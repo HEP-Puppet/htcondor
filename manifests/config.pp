35078c4f22068d14dd28a0d60f15b98c7ce165e2
Luke Kreczko <lkreczko@gmail.com>
2013-11-12 13:54:46 +0000

    sorting the order of parameters and splitting security config into seperate class

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,29 +2,27 @@
 #
 # Configuration deployment for HTCondor
 class htcondor::config (
-  $is_worker          = false,
-  $is_ce              = false,
-  $is_manager         = false,
+  $allow_write        = [],
+  $collector_name     = 'Personal Condor at $(FULL_HOSTNAME)',
+  $computing_elements = [],
+  $condor_admin_email = 'root@mysite.org',
   $condor_host        = $fqdn,
   # use if condor host has two NICs
   # and only the private should be used for condor
   $condor_host_ip     = '',
-  $condor_admin_email = 'root@mysite.org',
-  $collector_name     = 'Personal Condor at $(FULL_HOSTNAME)',
+  $is_ce              = false,
+  $is_manager         = false,
+  $is_worker          = false,
   $machine_owner      = 'physics',
+  $managers           = [],
   $number_of_cpus     = 8,
-  $allow_write        = [],
   $uid_domain         = 'example.com',
   # specify the networks with write access i.e. ["10.132.0.*"]
-  $managers           = [],
-  $computing_elements = [],
   $worker_nodes       = [],) {
-  $now                 = strftime('%d.%m.%Y_%H.%M')
-  $ce_daemon_list      = ['SCHEDD']
-  $worker_daemon_list  = ['STARTD']
-  $manage_daemon_list  = [
-    'COLLECTOR',
-    'NEGOTIATOR']
+  $now = strftime('%d.%m.%Y_%H.%M')
+  $ce_daemon_list = ['SCHEDD']
+  $worker_daemon_list = ['STARTD']
+  $manage_daemon_list = ['COLLECTOR', 'NEGOTIATOR']
 
   # default daemon, runs everywhere
   $default_daemon_list = ['MASTER']
@@ -34,40 +32,30 @@ class htcondor::config (
     File['/etc/condor/config.d/10_security.config'],
     ]
 
-  # complex preparation of manager, computing_element and worker_nodes lists
-  $managers_with_uid_domain           = prefix($managers, '*@$(UID_DOMAIN)/')
-  $computing_elements_with_uid_domain = prefix($computing_elements, '*@$(UID_DOMAIN)/'
-  )
-  $worker_nodes_with_uid_domain       = prefix($worker_nodes, '*@$(UID_DOMAIN)/'
-  )
-
   if $is_ce and $is_manager {
     # machine is both CE and manager (for small sites)
-    $temp_list               = concat($default_daemon_list, $ce_daemon_list)
-    $daemon_list             = concat($temp_list, $manage_daemon_list)
+    $temp_list = concat($default_daemon_list, $ce_daemon_list)
+    $daemon_list = concat($temp_list, $manage_daemon_list)
     $additional_config_files = [
       File['/etc/condor/config.d/12_resourcelimits.config'],
       File['/etc/condor/config.d/21_schedd.config'],
       File['/etc/condor/config.d/22_manager.config'],
       ]
-    $config_files            = concat($common_config_files, 
-    $additional_config_files)
+    $config_files = concat($common_config_files, $additional_config_files)
   } elsif $is_ce {
-    $daemon_list             = concat($default_daemon_list, $ce_daemon_list)
+    $daemon_list = concat($default_daemon_list, $ce_daemon_list)
     $additional_config_files = [
       File['/etc/condor/config.d/12_resourcelimits.config'],
       File['/etc/condor/config.d/21_schedd.config'],
       ]
-    $config_files            = concat($common_config_files, 
-    $additional_config_files)
+    $config_files = concat($common_config_files, $additional_config_files)
   } elsif $is_worker {
-    $daemon_list             = concat($default_daemon_list, $worker_daemon_list)
+    $daemon_list = concat($default_daemon_list, $worker_daemon_list)
     $additional_config_files = [File['/etc/condor/config.d/20_workernode.config'
         ],]
-    $config_files            = concat($common_config_files, 
-    $additional_config_files)
+    $config_files = concat($common_config_files, $additional_config_files)
   } else {
-    $daemon_list  = $default_daemon_list
+    $daemon_list = $default_daemon_list
     $config_files = $common_config_files
   }
 
@@ -85,15 +73,19 @@ class htcondor::config (
   # notify  => Service["condor"], this should be exec {'condor_reconfig':}
   }
 
+  class { 'htcondor::config::security':
+    computing_elements => $computing_elements,
+    managers           => $managers,
+    uid_domain         => $uid_domain,
+    worker_nodes       => $worker_nodes,
+  }
+
   file { '/etc/condor/config.d/10_security.config':
     content => template("${module_name}/10_security.config.erb"),
     require => Package['condor'],
   }
 
-  file { [
-    '/pool',
-    '/pool/condor',
-    '/etc/condor/persistent']:
+  file { ['/pool', '/pool/condor', '/etc/condor/persistent']:
     ensure => directory,
     owner  => 'condor',
   }
