3156e2480e59988a5dd7da4723f49ec65c7c8c37
Luke Kreczko <lkreczko@gmail.com>
2014-02-21 15:10:29 +0000

    New features and fixes:  - simplification of configuration (merged and deleted config fragments)  - added groups (issue #4)  - added generic template for fair shares (issue #8)  - moved non-used config files to files/examples  - added desription of custom hacks  - added documentation

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,30 +1,96 @@
 # Class htcondor::config
 #
 # Configuration deployment for HTCondor
+#
+# == Parameters:
+#
+# [*accounting_groups*]
+# Accounting grous (and subgroups) for fair share configuration.
+# Requires use_accounting_groups = true
+# Default just provides an example for what needs to be specified
+#
+# [*cluster_has_multiple_domains*]
+# Specifies if the cluster has more than one domain. If true it will set
+# TRUST_UID_DOMAIN = True in 10_security.config
+# Default: false
+#
+# [*collector_name*]
+# Sets COLLECTOR_NAME in 22_manager.config
+# Default: 'Personal Condor at $(FULL_HOSTNAME)'
+#
+# [*computing_elements*]
+# List of CEs that have access to this HTCondor pool
+#
+# [*condor_admin_email*]
+# Contact email for the pool admin. Sets CONDOR_ADMIN.
+#
+# [*custom_attribute*]
+# Can be used to specify a ClassAd via custom_attribute = True. This is useful
+# when creating queues with ARC CEs
+# Default: NORDUGRID_QUEUE
+#
+# [*is_ce*]
+# If machine is a computing element or a scheduler (condor term)
+#
+# [*is_manager*]
+# If machine is a manager or a negotiator (condor term)
+#
+# [*is_worker*]
+# If the machine is a worker node
+#
+# [*machine_owner*]
+# The owner of the machine (e.g. an accounting group)
+#
+# [*managers*]
+# List of condor managers
+#
+# [*number_of_cpus*]
+# Number of CPUs available for condor scheduling. This is set for worker nodes
+# only
+#
+# [*pool_password*]
+# Path to pool password file.
+#
+# [*uid_domain*]
+# Condor UID_DOMAIN
+# Default: example.com
+#
+# [*use_accounting_groups*]
+# If accounting groups should be used (fair shares)
+#
+# [*worker_nodes*]
+# List of worker nodes
+#
 class htcondor::config (
-  $allow_write        = [],
+  $accounting_groups            = {
+    'CMS'            => {
+      priority_factor => 10000.00,
+      dynamic_quota   => 0.80,
+    }
+    ,
+    'CMS.production' => {
+      priority_factor => 10000.00,
+      dynamic_quota   => 0.95,
+    }
+  }
+  ,
   $cluster_has_multiple_domains = false,
-  $collector_name     = 'Personal Condor at $(FULL_HOSTNAME)',
-  $computing_elements = [],
-  $condor_admin_email = 'root@mysite.org',
-  $condor_host        = $fqdn,
+  $collector_name = 'Personal Condor at $(FULL_HOSTNAME)',
+  $computing_elements           = [],
+  $condor_admin_email           = 'root@mysite.org',
+  $custom_attribute             = 'NORDUGRID_QUEUE',
+  $is_ce          = false,
+  $is_manager     = false,
+  $is_worker      = false,
+  $machine_owner  = 'physics',
+  $managers       = [],
+  $number_of_cpus = 8,
   # pool_password can also be served from central file location using hiera
-  $pool_password      = 'puppet:///modules/${module_name}/pool_password',
-  # use if condor host has two NICs
-  # and only the private should be used for condor
-  # use if condor host has two NICs
-  # and only the private should be used for condor
-  $condor_host_ip     = '',
-  $custom_attribute   = 'NORDUGRID_QUEUE',
-  $is_ce              = false,
-  $is_manager         = false,
-  $is_worker          = false,
-  $machine_owner      = 'physics',
-  $managers           = [],
-  $number_of_cpus     = 8,
-  $uid_domain         = 'example.com',
+  $pool_password  = "puppet:///modules/${module_name}/pool_password",
+  $uid_domain     = 'example.com',
+  $use_accounting_groups        = false,
   # specify the networks with write access i.e. ["10.132.0.*"]
-  $worker_nodes       = [],) {
+  $worker_nodes   = [],) {
   $now                 = strftime('%d.%m.%Y_%H.%M')
   $ce_daemon_list      = ['SCHEDD']
   $worker_daemon_list  = ['STARTD']
@@ -84,15 +150,11 @@ class htcondor::config (
     backup  => ".bak.${now}",
     content => template("${module_name}/condor_config.local.erb"),
     require => Package['condor'],
-  # notify  => Service["condor"], this should be exec {'condor_reconfig':}
   }
 
-  class { 'htcondor::config::security':
-    cluster_has_multiple_domains => $cluster_has_multiple_domains,
-    computing_elements           => $computing_elements,
-    managers                     => $managers,
-    uid_domain                   => $uid_domain,
-    worker_nodes                 => $worker_nodes,
+  file { '/etc/condor/config.d/10_security.config':
+    content => template("${module_name}/10_security.config.erb"),
+    require => Package['condor'],
   }
 
   file { ['/pool', '/pool/condor', '/etc/condor/persistent']:
@@ -102,13 +164,13 @@ class htcondor::config (
 
   file { '/etc/condor/pool_password':
     ensure => present,
-    source => "$pool_password",
+    source => $pool_password,
   }
 
   # files for certain roles
   if $is_ce {
     file { '/etc/condor/config.d/12_resourcelimits.config':
-      source  => "puppet:///modules/${module_name}/12_resourcelimits.config",
+      content => template("${module_name}/12_resourcelimits.config.erb"),
       require => Package['condor'],
     }
 
@@ -120,11 +182,13 @@ class htcondor::config (
   }
 
   if $is_manager {
-    # TODO: fair shares
-    file { '/etc/condor/config.d/11_fairshares.config':
-      content => template("${module_name}/11_fairshares.config.erb"),
-      require => Package['condor'],
+    if $use_accounting_groups {
+      file { '/etc/condor/config.d/11_fairshares.config':
+        content => template("${module_name}/11_fairshares.config.erb"),
+        require => Package['condor'],
+      }
     }
+
     file { '/etc/condor/config.d/22_manager.config':
       content => template("${module_name}/22_manager.config.erb"),
       require => Package['condor'],
