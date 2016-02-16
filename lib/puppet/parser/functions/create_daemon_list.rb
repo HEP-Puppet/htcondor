38d1e6bc1a19808d25e2527776aee141d476a6ff
kreczko <lkreczko@googlemail.com>
2016-02-16 12:53:59 +0000

    added tests for daemon list creation and fixed bug in the same

--- lib/puppet/parser/functions/create_daemon_list.rb
+++ lib/puppet/parser/functions/create_daemon_list.rb
@@ -9,13 +9,6 @@ module Puppet::Parser::Functions
     ganglia = args[4]
     high_availability= args[5]
 
-    default_list = ['MASTER']
-    worker_daemon_list  = ['STARTD']
-    sched_daemon_list     = ['SCHEDD']
-    manage_daemon_list = ['COLLECTOR', 'NEGOTIATOR']
-    ganglia_daemon_list   = ['GANGLIAD']
-    #HAD, REPLICATION
-
     daemon_list = Array.new
     # all nodes have master
     daemon_list.push 'MASTER'
@@ -30,15 +23,15 @@ module Puppet::Parser::Functions
       daemon_list.push 'COLLECTOR'
       daemon_list.push 'NEGOTIATOR'
     end
-    if defrag == true
+    if defrag == true and is_manager == true
       daemon_list.push 'DEFRAG'
     end
-    if ganglia == true
+    if ganglia == true and (is_manager == true or is_scheduler == true)
       daemon_list.push 'GANGLIAD'
     end
-    if high_availability == true
-      high_availability.push 'HAD'
-      high_availability.push 'REPLICATION'
+    if high_availability == true and is_manager == true
+      daemon_list.push 'HAD'
+      daemon_list.push 'REPLICATION'
     end
 
     return daemon_list.join(", ")
