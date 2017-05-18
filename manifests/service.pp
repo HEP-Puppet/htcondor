a3e2a04d24e7500be442a010c6c74f76ac23c3d1
kreczko <lkreczko@googlemail.com>
2017-05-18 17:01:01 +0100

    fixing rspec warnings

--- manifests/service.pp
+++ manifests/service.pp
@@ -8,9 +8,11 @@ class htcondor::service {
     hasrestart => true,
     hasstatus  => true,
   }
-  ->
-  #this exec is called from the config, but we can't run it if the condor service is not up. it's a RE-config command assuming something is already up.
+
+  # this exec is called from the config, but we can't run it if the condor
+  # service is not up. it's a RE-config command assuming something is already up.
   exec{ '/usr/sbin/condor_reconfig':
     refreshonly => true,
+    require     => Service['condor']
   }
 }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
+require 'rspec-puppet-utils'
+require 'rspec/mocks'
+require 'rspec-puppet-facts'
 
 # hack to enable all the expect syntax (like allow_any_instance_of) in rspec-puppet examples
 RSpec::Mocks::Syntax.enable_expect(RSpec::Puppet::ManifestMatchers)
@@ -6,7 +9,6 @@ RSpec::Mocks::Syntax.enable_expect(RSpec::Puppet::ManifestMatchers)
 RSpec.configure do |c|
   c.add_setting :puppet_future
   c.puppet_future = Puppet.version.to_f >= 4.0
-  c.treat_symbols_as_metadata_keys_with_true_values = true
 
   c.before :each do
     # Ensure that we don't accidentally cache facts and environment
